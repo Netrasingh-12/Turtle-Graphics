d0fbbe1b351718b16f3412b76e45156280faa8c9
Netrasingh-12 <89205750+Netrasingh-12@users.noreply.github.com>
2022-01-06 19:50:56 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,8 @@
+import turtle
+
+cir = turtle.Turtle()
+
+cir.circle(100)
+
+
+turtle.done()
---
+++ triangle.py
@@ -0,0 +1,10 @@
+import turtle
+
+triangle = turtle.Turtle()
+
+for i in range(3):
+    triangle.forward(300)
+    triangle.left(120)
+
+
+turtle.done()
